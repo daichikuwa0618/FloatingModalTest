60046330dbc5ae592180ca253c4ab57924d892c5
Daichi Hayashi <daichikuwa0618@gmail.com>
2020-04-17 21:42:20 +0900

    extension PanModalPresentable

--- panModalTest/panModalTest/ViewController.swift
+++ panModalTest/panModalTest/ViewController.swift
@@ -15,7 +15,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+}
 
+extension ViewController: PanModalPresentable {
 
+    var panScrollable: UIScrollView? {
+        return nil
+    }
 }
 

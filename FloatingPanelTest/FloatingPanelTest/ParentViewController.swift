cd2bff037e84becdabe44f5330f50a38720baba4
Daichi Hayashi <daichikuwa0618@gmail.com>
2020-04-17 22:00:35 +0900

    ParentVC に FloatingPanel を表示する処理を追加

--- FloatingPanelTest/FloatingPanelTest/ParentViewController.swift
+++ FloatingPanelTest/FloatingPanelTest/ParentViewController.swift
@@ -7,14 +7,29 @@
 //
 
 import UIKit
+import FloatingPanel
 
 class ParentViewController: UIViewController {
 
+    var floatingPanelController: FloatingPanelController!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        floatingPanelController = FloatingPanelController()
+
+        let floatingPanelViewController = FloatingPanelViewController()
+        floatingPanelController.set(contentViewController: floatingPanelViewController)
+
+        // FloatingPanel の表示
+        floatingPanelController.addPanel(toParent: self, belowView: nil, animated: false)
     }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
 
+        // FloatingPanel を非表示
+        floatingPanelController.removePanelFromParent(animated: true)
+    }
 }
 

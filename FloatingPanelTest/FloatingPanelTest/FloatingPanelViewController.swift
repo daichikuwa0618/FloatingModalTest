b2187f25d020e29688d98f31b66103db7142cabd
Daichi Hayashi <daichikuwa0618@gmail.com>
2020-04-17 21:52:41 +0900

    FloatingPanel を実装する VC を追加 (背景色は適当)

---
+++ FloatingPanelTest/FloatingPanelTest/FloatingPanelViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FloatingPanelViewController.swift
+//  FloatingPanelTest
+//
+//  Created by 林 大地 on 2020/04/17.
+//  Copyright © 2020 Daichi Hayashi. All rights reserved.
+//
+
+import UIKit
+
+class FloatingPanelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.orange
+    }
+}
